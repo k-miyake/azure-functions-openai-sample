2011610d3cdffb50ffaf0ec4b93badba76b99498
Kazuyuki Miyake <k-miyake@zengeeks.io>
2023-05-13 15:52:28 +0900

    Add parameters to ChatGPT API request

--- invoke-chatgpt/index.ts
+++ invoke-chatgpt/index.ts
@@ -1,17 +1,39 @@
-import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import { Context, HttpRequest } from "@azure/functions";
 
-const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    context.log('HTTP trigger function processed a request.');
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+const params = {
+  messages: [
+    {
+      role: "user",
+      content: "何か面白いことを言ってください",
+    },
+  ],
+  temperature: 0.7,
+  top_p: 0.95,
+  frequency_penalty: 0,
+  presence_penalty: 0,
+  max_tokens: 800,
+  stop: null,
+};
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
+// HTTPトリガーを処理するAzure Function
+const httpTrigger = async function (context: Context): Promise<void> {
+  const url = process.env.OPENAI_ENDPOINT;
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "api-key": process.env.OPENAI_API_KEY,
+    },
+    body: JSON.stringify(params),
+  };
 
+  const res = await fetch(url, options);
+  const completion = await res.json();
+
+  // ChatGPT APIからの応答をHTTPレスポンスのレスポンスボディとして返す
+  context.res = {
+    body: completion.choices[0].message.content,
+  };
 };
 
 export default httpTrigger;
